97041fc5f2a36cfc7bcf7a168c7b05a69157fa0d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-10 03:16:40 +0500

    divided into multiple components

--- components/Auth/SignUp.tsx
+++ components/Auth/SignUp.tsx
@@ -1,25 +1,34 @@
-// pages/signup.tsx
+// components/SignupForm.tsx
 import { FormEvent, useState } from 'react'
-
-export default function Signup() {
-  const [username, setUsername] = useState<string>('')
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [confirmPassword, setConfirmPassword] = useState<string>('')
+import InputField from './inputfield'
+import FileUpload from './filefield'
+import SubmitButton from './submitbtn'
+import { inputFields } from '@/utils/InputfieldArray'
+export default function SignupForm() {
+  const [formValues, setFormValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
   const [image, setImage] = useState<File | null>(null)
-
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [id]: value,
+    }))
+  }
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       setImage(e.target.files[0])
     }
   }
-
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     // Add signup functionality here
     // Handle image upload and form submission
   }
-
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#F8F9FA]">
       <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
@@ -27,99 +36,20 @@ export default function Signup() {
           Sign Up
         </h2>
         <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-dark-gray"
-            >
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-[#007BFF]"
+          {inputFields.map(({ id, type, label }) => (
+            <InputField
+              key={id}
+              id={id}
+              type={type}
+              label={label}
+              value={formValues[id as keyof typeof formValues]}
+              onChange={handleChange}
               required
             />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-dark-gray"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-[#007BFF]"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-dark-gray"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-[#007BFF]"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="confirm-password"
-              className="block text-sm font-medium text-dark-gray"
-            >
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirm-password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-[#007BFF]"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="image"
-              className="block text-sm font-medium text-dark-gray"
-            >
-              Profile Image
-            </label>
-            <div className="relative">
-              <input
-                type="file"
-                id="image"
-                onChange={handleImageChange}
-                className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-              />
-              <button
-                type="button"
-                className="w-full py-2 px-4 bg-[#007BFF] text-white rounded-md hover:bg-[#0056b3] flex items-center justify-center"
-              >
-                {image ? image.name : 'Choose File'}
-              </button>
-            </div>
-          </div>
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-[#007BFF] text-white rounded-md hover:bg-[#0056b3]"
-          >
-            Sign Up
-          </button>
-          <div className="mt-4 text-center">
+          ))}
+          <FileUpload id="image" file={image} onChange={handleImageChange} />
+          <SubmitButton text="Sign Up" />
+          <div className="mt-4 text-center text-black">
             <p>
               Already have an account?{' '}
               <a href="/login" className="text-[#007BFF] hover:underline">
