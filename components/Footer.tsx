517a932421ad3069626924415ffdda57d557e9ba
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 16:56:38 +0500

    styled done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-[#080303] text-[#343A40] p-1  fixed bottom-0 w-full">
+    <footer className="bg-[#01000000] text-[#343A40]  mb-1  fixed bottom-0 w-full">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 TaskNest. All rights reserved.</p>
       </div>
