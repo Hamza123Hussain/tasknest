56e48a1afdb2a5a99a31ff407575ee67be965215
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 03:39:56 +0500

    usereducer added in store

--- utils/Redux/Store.ts
+++ utils/Redux/Store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { TodoReducer } from './Slices/TODO/Slice/TodoSlice'
+import { UserReducer } from './Slices/User/UserSlice'
+
 export const store = configureStore({
   reducer: {
     TodoReducer,
+    UserReducer,
   },
 })
 // Infer the `RootState` and `AppDispatch` types from the store itself
