fe18ad8fbb0ac8f9423223e4b94bf4684976822e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 18:42:30 +0500

    remove function enabled

--- components/Task/ShowTask.tsx
+++ components/Task/ShowTask.tsx
@@ -1,25 +1,29 @@
 'use client'
+import { RemoveTask } from '@/utils/Redux/Slices/TodoSlice'
 import { RootState } from '@/utils/Redux/Store'
 import React from 'react'
-import { useSelector } from 'react-redux'
-
+import { FaRegTrashAlt } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
 const ShowTask = () => {
   // Typing useSelector to select the todo from the store
   const tasks = useSelector((state: RootState) => state.TodoReducer.todo)
-
+  const Dispatch = useDispatch()
   return (
     <div className="bg-[#F8F9FA] p-6 rounded-lg shadow-md">
       <h2 className="text-[#007BFF] text-2xl font-bold mb-4">Tasks</h2>
       {tasks.map((task) => (
         <div
           key={task.id}
-          className="bg-white text-gray-800 p-3 my-2 rounded-md border border-[#007BFF] shadow-sm"
+          className="bg-white text-gray-800 p-3 flex items-center justify-between my-2 rounded-md border border-[#007BFF] shadow-sm"
         >
-          {task.Text}
+          <span> {task.Text}</span>
+          <FaRegTrashAlt
+            className=" text-red-700 cursor-pointer"
+            onClick={() => Dispatch(RemoveTask(task.id))}
+          />
         </div>
       ))}
     </div>
   )
 }
-
 export default ShowTask
