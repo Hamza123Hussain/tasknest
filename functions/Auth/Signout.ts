cb30fc8d3c9c241bc9fb8addb6976514ce198d6d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 04:47:11 +0500

    little error

---
+++ functions/Auth/Signout.ts
@@ -0,0 +1,19 @@
+// utils/api.ts
+import { ApiUrl } from '@/utils/SignUpInterface'
+import axios from 'axios'
+export const signOut = async () => {
+  try {
+    const response = await axios.post<boolean>(`${ApiUrl}api/Auth/Signout`)
+    if (response.status === 200) {
+      return true
+      // Handle success, such as redirecting to the login page
+      window.location.href = '/login' // Redirect to login page after sign-out
+    }
+  } catch (error) {
+    console.error(
+      'Error during sign-out:',
+      (error as Error).message || 'Unexpected error'
+    )
+    // Handle error, such as showing an error message
+  }
+}
