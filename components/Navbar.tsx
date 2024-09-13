32b6ebbedc0ea6d079a30997eeacb90af7e312a6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 03:38:55 +0500

    removed unused list

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,23 +9,8 @@ const Navbar = () => {
         </a>
         <ul className="flex space-x-6">
           <li>
-            <a href="#features" className="hover:text-[#28A745]">
-              Features
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="hover:text-[#28A745]">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="hover:text-[#28A745]">
-              Contact
-            </a>
-          </li>
-          <li>
             <a
-              href="#login"
+              href="/login"
               className="bg-[#FFC107] text-dark-gray px-4 py-2 rounded hover:bg-[#e0a800]"
             >
               Login
