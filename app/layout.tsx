08cbe23fe022ad0e80495a13601e78d82759bc43
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 16:56:59 +0500

    fixed layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import ReduxComp from '@/components/ReduxComp'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -30,9 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
-        <Footer />
+        <ReduxComp>{children}</ReduxComp>
       </body>
     </html>
   )
