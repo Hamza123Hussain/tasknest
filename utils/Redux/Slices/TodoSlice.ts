947feb622dea56a3c465d0a575e85f901746355f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 15:50:12 +0500

    first slice with actions has been made

--- utils/Redux/Slices/TodoSlice.ts
+++ utils/Redux/Slices/TodoSlice.ts
@@ -1,9 +1,22 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
 
-const initialState = { todo: [] }
+const initialState = { todo: [{ id: nanoid(), Text: 'MY NAME IS HAMZA' }] }
 
 export const TodoSlice = createSlice({
   name: 'TodoSlice',
   initialState,
-  reducers: {},
+  reducers: {
+    AddTask: (state, action) => {
+      const NewElement = { id: nanoid(), Text: action.payload }
+      state.todo.push(NewElement)
+    },
+    RemoveTask: (state, action) => {
+      state.todo = state.todo.filter((element) => {
+        element.id !== action.payload
+      })
+    },
+  },
 })
+
+export const { AddTask, RemoveTask } = TodoSlice.actions
+export const TodoReducer = TodoSlice.reducer
