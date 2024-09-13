b5745d6110f5accd07d2d22bd02b21c1ea1edb96
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 04:46:44 +0500

    added home href

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,9 @@ import { RootState } from '@/utils/Redux/Store'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useRouter } from 'next/navigation'
-import ShowUser from './ShowUser'
+import ShowUser from './Auth/ShowUser'
+import Image from 'next/image'
+import Logo from '../public/Logo.png'
 const Navbar: React.FC = () => {
   const User = useSelector((state: RootState) => state.UserReducer)
   const router = useRouter()
@@ -13,8 +15,9 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-[#007BFF] text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="#" className="text-2xl font-bold">
-          TaskNest
+        <a href="/" className="text-2xl font-bold flex items-center">
+          <Image src={Logo} alt="Log" width={50} height={20} />
+          <span>TaskNest</span>
         </a>
         <ul className="flex space-x-6 items-center">
           {User._id ? (
