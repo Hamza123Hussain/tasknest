27e62fc13b02598e3b152dd3659d3dbc1d10692d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 04:21:45 +0500

    showing user when they log in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,37 @@
+// components/Navbar.tsx
 import { RootState } from '@/utils/Redux/Store'
 import React from 'react'
 import { useSelector } from 'react-redux'
-
-const Navbar = () => {
+import { useRouter } from 'next/navigation'
+import ShowUser from './ShowUser'
+const Navbar: React.FC = () => {
   const User = useSelector((state: RootState) => state.UserReducer)
+  const router = useRouter()
+  const handleLoginClick = () => {
+    router.push('/login')
+  }
   return (
     <nav className="bg-[#007BFF] text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <a href="#" className="text-2xl font-bold">
           TaskNest
         </a>
-        <ul className="flex space-x-6">
-          <li>
-            <a
-              href="/login"
-              className="bg-[#FFC107] text-dark-gray px-4 py-2 rounded hover:bg-[#e0a800]"
-            >
-              {User.name}
-            </a>
-          </li>
+        <ul className="flex space-x-6 items-center">
+          {User._id ? (
+            <ShowUser />
+          ) : (
+            <li>
+              <button
+                onClick={handleLoginClick}
+                className="bg-[#FFC107] text-dark-gray px-4 py-2 rounded hover:bg-[#e0a800]"
+              >
+                Login
+              </button>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
   )
 }
-
 export default Navbar
