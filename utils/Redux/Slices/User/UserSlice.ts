90317a6cdca0cc386874985f296c4fe017bbfa6d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 03:53:05 +0500

    user slice made

--- utils/Redux/Slices/User/UserSlice.ts
+++ utils/Redux/Slices/User/UserSlice.ts
@@ -1,25 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit'
-const initialState = {
+import { UserData } from '@/utils/SignUpInterface'
+import { loadFromLocalStorage, saveToLocalStorage } from './LocalStorage'
+const initialState: UserData = {
   createdAt: '',
   email: '',
   imageUrl: '',
   name: '',
   _id: '',
 }
-
+// Initialize state from local storage
+const initialStateFromLocalStorage = loadFromLocalStorage()
 export const UserSlice = createSlice({
   name: 'User',
-  initialState,
+  initialState: initialStateFromLocalStorage,
   reducers: {
-    UserData: (state, action) => {
-      ;(state._id = action.payload._id),
-        (state.name = action.payload.name),
-        (state.createdAt = action.payload.createdAt),
-        (state.email = action.payload.email),
-        (state.imageUrl = action.payload.imageUrl)
+    UserMade: (state, action) => {
+      state._id = action.payload._id
+      state.name = action.payload.name
+      state.createdAt = action.payload.createdAt
+      state.email = action.payload.email
+      state.imageUrl = action.payload.imageUrl
+
+      // Save updated state to local storage
+      saveToLocalStorage(state)
+    },
+    clearUserData: (state) => {
+      // Clear state
+      Object.assign(state, initialState)
+      // Clear local storage
+      localStorage.removeItem('UserData')
     },
   },
 })
 
-export const { UserData } = UserSlice.actions
+export const { UserMade, clearUserData } = UserSlice.actions
 export const UserReducer = UserSlice.reducer
