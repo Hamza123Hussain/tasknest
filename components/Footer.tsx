6e13e74204a2ae590a011a49bc859b385230d343
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 16:33:44 +0500

    removed unnesscary text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,21 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-[#080303] text-[#343A40] p-6 mt-12 fixed bottom-0 w-full">
+    <footer className="bg-[#080303] text-[#343A40] p-1  fixed bottom-0 w-full">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 TaskNest. All rights reserved.</p>
-        <ul className="flex justify-center space-x-4 mt-4">
-          <li>
-            <a href="#privacy" className="hover:text-[#28A745]">
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href="#terms" className="hover:text-[#28A745]">
-              Terms of Service
-            </a>
-          </li>
-        </ul>
       </div>
     </footer>
   )
