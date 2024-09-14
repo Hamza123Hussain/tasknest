02818fd780ea60dd9aff7bc5247bafd4101d70d4
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-15 03:07:32 +0500

    data types added

--- utils/TodoInterface.ts
+++ utils/TodoInterface.ts
@@ -25,3 +25,9 @@ export interface TaskModalProps {
   onSubmitType: string
   ID?: string
 }
+
+export interface ModalBodyProps {
+  onClose: () => void // Function to close the modal
+  onSubmitType: string // Prop to determine which function to call
+  todoId?: string // Optional todoId for adding a task
+}
