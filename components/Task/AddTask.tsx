060f4969255cb4a36734e59c42a5805dbd91fa52
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 16:39:28 +0500

    changing text and giving props to the modal

--- components/Task/AddTask.tsx
+++ components/Task/AddTask.tsx
@@ -1,20 +1,22 @@
 'use client'
 import React, { useState } from 'react'
 import TaskModal from './TaskModal'
-
 const TaskCreater = () => {
   const [isModalOpen, setModalOpen] = useState(false)
-
   return (
     <>
       <button
         onClick={() => setModalOpen(true)}
-        className="px-4 py-2 rounded-md bg-[#007BFF] text-white hover:bg-[#0056b3]"
+        className="px-4 py-2 rounded-md w-48 bg-[#007BFF] text-white hover:bg-[#0056b3]"
       >
-        Create Task
+        Create Element
       </button>
-
-      <TaskModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
+      <TaskModal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        Name="Create New Element"
+        onSubmitType="element"
+      />
     </>
   )
 }
