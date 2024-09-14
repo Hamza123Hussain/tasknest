384f580dc83bb3ad54ab9da47e87887b00bb8e01
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 16:38:48 +0500

    showing text when no tasks are added

--- components/Task/ShowTask.tsx
+++ components/Task/ShowTask.tsx
@@ -5,12 +5,19 @@ import MainCard from './MainCard'
 
 const ShowTask = () => {
   const todos = useSelector((state: RootState) => state.TodoReducer.todo)
+
   return (
-    <div className="bg-[#F8F9FA] p-6 rounded-lg shadow-md">
-      <h2 className="text-[#007BFF] text-6xl font-bold mb-4">Tasks</h2>
-      {todos.map((todo) => (
-        <MainCard todo={todo} key={todo.id} />
-      ))}
+    <div className="my-5">
+      <h2 className="text-[#007BFF] text-5xl font-bold mb-3">Task List</h2>
+      <div className="p-4 flex flex-col gap-5 w-[70vw] mx-auto rounded-lg shadow-lg">
+        {todos.length === 0 ? (
+          <p className="text-gray-500 text-lg text-center my-10 ">
+            No tasks available. Please add a new task to get started!
+          </p>
+        ) : (
+          todos.map((todo) => <MainCard todo={todo} key={todo.id} />)
+        )}
+      </div>
     </div>
   )
 }
