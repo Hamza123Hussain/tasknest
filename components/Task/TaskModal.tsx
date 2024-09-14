bce177106e75af3868867fc8d44e496c7bab5395
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 16:38:36 +0500

    modal fixed

--- components/Task/TaskModal.tsx
+++ components/Task/TaskModal.tsx
@@ -1,13 +1,19 @@
 'use client'
 import { TaskModalProps } from '@/utils/TodoInterface'
 import ModalBody from './ModelBody'
-const TaskModal: React.FC<TaskModalProps> = ({ isOpen, onClose }) => {
+const TaskModal: React.FC<TaskModalProps> = ({
+  isOpen,
+  onClose,
+  Name,
+  onSubmitType,
+  ID,
+}) => {
   if (!isOpen) return null // Don't render the modal if it's not open
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md">
       <div className="bg-[#ffffff] rounded-lg shadow-lg w-full max-w-lg p-6">
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-bold text-black">Create a New Task</h2>
+          <h2 className="text-xl font-bold text-black">{Name}</h2>
           <button
             className="text-black hover:text-gray-300 focus:outline-none"
             onClick={onClose}
@@ -28,7 +34,7 @@ const TaskModal: React.FC<TaskModalProps> = ({ isOpen, onClose }) => {
             </svg>
           </button>
         </div>
-        <ModalBody onClose={onClose} />
+        <ModalBody onClose={onClose} onSubmitType={onSubmitType} todoId={ID} />
       </div>
     </div>
   )
