68802c2461e14d4ce76a643889ac633add110d88
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-15 03:07:02 +0500

    fixed

--- functions/Task/UpdateTask.ts
+++ functions/Task/UpdateTask.ts
@@ -1,21 +1,22 @@
+import { ApiUrl } from '@/utils/SignUpInterface'
 import axios from 'axios'
-
 export const updateTask = async (
   elementId: string,
   taskId: string,
-  ISDone: boolean
+  Isdone: boolean
 ) => {
   try {
-    const response = await axios.put(
-      `/api/task/update/${elementId}/${taskId}`,
+    const response = await axios.patch(
+      ` ${ApiUrl}api/Task/UpdateTask`,
+      { Isdone },
       {
-        isDone: ISDone, // Updated task completion status
+        params: { elementId, taskId },
       }
     )
     console.log('Task updated:', response.data)
-    return response.data
+    return response.data // Return the updated task or element data
   } catch (error) {
     console.error('Error updating task:', error)
-    throw error
+    throw error // Rethrow error for further handling
   }
 }
