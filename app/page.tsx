f55b629d85e579c5e5817c7da6e76fdaf87ebe7a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 16:38:02 +0500

    fixed styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ShowTask from '@/components/Task/ShowTask'
 
 export default function Home() {
   return (
-    <div className="mt-4 flex gap-5 flex-col bg-gray-900 rounded-lg p-4">
+    <div className="mt-4 flex gap-5 flex-col  rounded-lg p-4">
       <TaskCreater />
       <ShowTask />
     </div>
