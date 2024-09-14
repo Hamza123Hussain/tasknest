8c4e91cf4306c5327943df5909ed63246bb5f934
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 15:25:52 +0500

    little bit image size fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,22 +15,26 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-[#007BFF] text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-2xl font-bold flex items-center">
-          <Image src={Logo} alt="Log" width={50} height={20} />
+        <a href="/" className="text-2xl font-bold flex items-center gap-2">
+          <Image
+            className=" rounded-full bg-transparent"
+            src={Logo}
+            alt="Log"
+            width={25}
+            height={20}
+          />
           <span>TaskNest</span>
         </a>
         <ul className="flex space-x-6 items-center">
           {User._id ? (
             <ShowUser />
           ) : (
-            <li>
-              <button
-                onClick={handleLoginClick}
-                className="bg-[#FFC107] text-dark-gray px-4 py-2 rounded hover:bg-[#e0a800]"
-              >
-                Login
-              </button>
-            </li>
+            <button
+              onClick={handleLoginClick}
+              className="bg-[#FFC107] text-dark-gray px-4 py-2 rounded hover:bg-[#e0a800]"
+            >
+              Login
+            </button>
           )}
         </ul>
       </div>
