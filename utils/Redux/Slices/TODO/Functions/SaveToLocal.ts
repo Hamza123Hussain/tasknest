270b6a6a7814e5abdaaffbfa1c491fce88af0524
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-15 19:17:00 +0500

    not needed

--- utils/Redux/Slices/TODO/Functions/SaveToLocal.ts
+++
@@ -1,6 +0,0 @@
-import { Todo } from '@/utils/TodoInterface'
-
-// Helper function to save todos to localStorage
-export const saveToLocalStorage = (todos: Todo[]) => {
-  localStorage.setItem('todos', JSON.stringify(todos))
-}
