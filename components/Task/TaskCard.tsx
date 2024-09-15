3fba89a512ad332ce2b5efc6a6084f3729f615c9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-16 00:42:39 +0500

    fixed the task card

--- components/Task/TaskCard.tsx
+++ components/Task/TaskCard.tsx
@@ -1,19 +1,26 @@
+import { useDispatch } from 'react-redux'
+import { FaCheckCircle, FaRegCircle, FaTrash } from 'react-icons/fa'
+import { deleteTask } from '@/functions/Task/DeleteTask'
 import {
   CompleteTask,
   RemoveATask,
 } from '@/utils/Redux/Slices/TODO/Slice/TodoSlice'
+import { AppDispatch } from '@/utils/Redux/Store'
 import { Task } from '@/utils/TodoInterface'
-import React from 'react'
-import { FaCheckCircle, FaRegCircle, FaTrash } from 'react-icons/fa'
-import { useDispatch } from 'react-redux'
-
 const TaskCard = ({ task, ID }: { task: Task; ID: string }) => {
-  const dispatch = useDispatch()
+  console.log('Taskss : ', task._id)
+  const dispatch = useDispatch<AppDispatch>()
+  const handleDelete = async () => {
+    dispatch(RemoveATask({ todoId: ID, taskID: task._id }))
+    const response = await deleteTask(ID, task._id)
+    if (response.message === 'Task deleted successfully') {
+      alert('item gone')
+    }
+  }
   return (
     <div
-      key={task.ID}
       className={`bg-white text-gray-800 p-3 flex flex-col mb-2 rounded-md border ${
-        task.ISDone ? 'border-green-500 bg-green-950' : 'border-[#007BFF]'
+        task.ISDone ? 'border-green-500 bg-blue-900' : 'border-[#007BFF]'
       } shadow-sm`}
     >
       <div className="flex items-center justify-between">
@@ -27,7 +34,7 @@ const TaskCard = ({ task, ID }: { task: Task; ID: string }) => {
         <div className=" flex justify-between items-center gap-3">
           <button
             onClick={() =>
-              dispatch(CompleteTask({ todoId: ID, taskId: task.ID }))
+              dispatch(CompleteTask({ todoId: ID, taskId: task._id }))
             }
             className="text-green-500 hover:text-green-600 ml-2"
             aria-label="Toggle Task Completion"
@@ -35,17 +42,15 @@ const TaskCard = ({ task, ID }: { task: Task; ID: string }) => {
             {task.ISDone ? <FaCheckCircle /> : <FaRegCircle />}
           </button>
           <FaTrash
-            className=" text-red-600 cursor-pointer"
-            onClick={() =>
-              dispatch(RemoveATask({ todoId: ID, taskID: task.ID }))
-            }
+            className="text-red-600 cursor-pointer"
+            onClick={handleDelete}
+            aria-label="Delete Task"
           />
         </div>
       </div>
       <div className="text-gray-600 text-sm mt-2">
         <p>Created At: {new Date(task.CreatedAt).toLocaleString()}</p>
       </div>
-      <div className="flex justify-end mt-2"></div>
     </div>
   )
 }
