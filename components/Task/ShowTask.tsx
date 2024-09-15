d40dacb909ef1d236ae440d8662ecb5467059a98
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-15 19:16:54 +0500

    using database to get todo

--- components/Task/ShowTask.tsx
+++ components/Task/ShowTask.tsx
@@ -1,10 +1,19 @@
-import { RootState } from '@/utils/Redux/Store'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '@/utils/Redux/Store'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import MainCard from './MainCard'
+import { fetchUserElements } from '@/utils/Redux/Slices/TODO/Slice/TodoSlice'
 
 const ShowTask = () => {
   const todos = useSelector((state: RootState) => state.TodoReducer.todo)
+  const User = useSelector((state: RootState) => state.UserReducer)
+  const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    if (User.email) {
+      dispatch(fetchUserElements(User.email)) // Fetch user elements when component mounts
+    }
+  }, [dispatch, User.email])
 
   return (
     <div className="my-5">
@@ -15,7 +24,7 @@ const ShowTask = () => {
             No tasks available. Please add a new task to get started!
           </p>
         ) : (
-          todos.map((todo) => <MainCard todo={todo} key={todo.id} />)
+          todos.map((todo) => <MainCard todo={todo} key={todo._id} />)
         )}
       </div>
     </div>
