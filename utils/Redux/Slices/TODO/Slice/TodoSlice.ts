017516b52db38bf61649900b3bc26865134d3008
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-15 19:17:35 +0500

    using thunk to to get items from db

--- utils/Redux/Slices/TODO/Slice/TodoSlice.ts
+++ utils/Redux/Slices/TODO/Slice/TodoSlice.ts
@@ -1,17 +1,25 @@
-// utils/Redux/Slices/TodoSlice.ts
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { TodoState } from '@/utils/TodoInterface'
+import { getAllUserElements } from '@/functions/Element/GetUserElements'
 import { AddTodo } from '../Functions/AddElement'
 import { AddTaskToTodo } from '../Functions/AddNewTask'
 import { ToggleTaskCompletion } from '../Functions/CompleteTask'
-import { RemoveTodo } from '../Functions/RemoveTask'
 import { RemoveTask } from '../Functions/RemoveATask'
-const loadFromLocalStorage = (): TodoState['todo'] => {
-  const savedTodos = localStorage.getItem('todos')
-  return savedTodos ? JSON.parse(savedTodos) : []
-}
+import { RemoveTodo } from '../Functions/RemoveTask'
+// Async action to fetch user elements from the API
+export const fetchUserElements = createAsyncThunk(
+  'todo/fetchUserElements',
+  async (userEmail: string, { rejectWithValue }) => {
+    try {
+      const data = await getAllUserElements(userEmail)
+      return data
+    } catch (error) {
+      return rejectWithValue(error)
+    }
+  }
+)
 const initialState: TodoState = {
-  todo: loadFromLocalStorage(),
+  todo: [], // Initial state is empty until we fetch data from the API
 }
 export const TodoSlice = createSlice({
   name: 'TodoSlice',
@@ -23,7 +31,16 @@ export const TodoSlice = createSlice({
     RemoveElement: RemoveTodo,
     RemoveATask: RemoveTask,
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserElements.fulfilled, (state, action) => {
+      state.todo = action.payload // Update state with API data
+    })
+    builder.addCase(fetchUserElements.rejected, (state, action) => {
+      console.error('Error fetching elements:', action.payload)
+    })
+  },
 })
+
 export const { NewElement, NewTask, CompleteTask, RemoveElement, RemoveATask } =
   TodoSlice.actions
 
