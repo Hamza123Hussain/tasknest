1d5b5f724cedccf1efcfcb90975df45afad4df4b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-16 01:12:03 +0500

    ALWAYS AT BOTTOM

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-[#01000000] text-[#343A40]  mb-1  fixed bottom-0 w-full">
-      <div className="container mx-auto text-center">
+    <footer className="bg-[#01000000] text-[#343A40] mb-1 w-full">
+      <div className="container mx-auto text-center py-4">
         <p>&copy; 2024 TaskNest. All rights reserved.</p>
       </div>
     </footer>
